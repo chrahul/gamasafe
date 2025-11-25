cc7181df5f62c7ef898ce379e20e2ac8fbe59783
Rahul's Home <rahulch.unix@gmail.com>
2025-11-25 17:55:37 +0530

    Update charts.py

--- dashboard/charts.py
+++ dashboard/charts.py
@@ -1,1 +1,23 @@
 
+"""
+charts.py
+-------------------------
+Chart helper functions for GamaSafe dashboard.
+
+TODO:
+- Add OI bar charts
+- Add IV trend
+- Add delta/gamma charts
+"""
+
+import streamlit as st
+import matplotlib.pyplot as plt
+
+def plot_placeholder_chart():
+    """
+    Temporary placeholder chart for early development.
+    """
+    fig, ax = plt.subplots()
+    ax.plot([1, 2, 3], [2, 3, 2])
+    ax.set_title("Placeholder Chart")
+    st.pyplot(fig)
